08edbbd93d91f58cc5963b55facc46af49c6466b
linonetwo <linonetwo012@gmail.com>
2018-08-21 15:45:53 +0800

    chore: init dev deps

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// Is the current build a development build
+const IS_DEV = (process.env.NODE_ENV === 'dev');
+
+const dirNode = 'node_modules';
+const dirApp = path.join(__dirname, 'src');
+
+/**
+ * Webpack Configuration
+ */
+module.exports = {
+    entry: {
+        vendor: [
+            'lodash'
+        ],
+        bundle: path.join(dirApp, 'index')
+    },
+    resolve: {
+        modules: [
+            dirNode,
+            dirApp,
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV: IS_DEV
+        }),
+    ],
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    compact: true
+                }
+            },
+        ]
+    }
+};
