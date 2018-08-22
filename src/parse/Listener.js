bc6a86c27c50b5022cc5c2ace70034083ff11db2
linonetwo <linonetwo012@gmail.com>
2018-08-22 17:29:36 +0800

    feat: construce concat pos

--- src/parse/Listener.js
+++ src/parse/Listener.js
@@ -4,8 +4,11 @@ import { TokenRegexItParser } from '../antlrGeneratedParser/TokenRegexItParser';
 import { TokenRegexItListener } from '../antlrGeneratedParser/TokenRegexItListener';
 
 export interface Marker {
-  firstPosition(position: number): void;
+  leaf(token: string | null, position: number, nullable?: boolean): void;
+  concat(leftPosition: number, rightPosition: number): void;
+  concatPositionIndex(leftPosition: number, rightPosition: number): string;
 }
+interface Symbol { tokenIndex: number; text: string }
 
 export default class Listener extends TokenRegexItListener {
   marker: Marker;
@@ -17,7 +20,27 @@ export default class Listener extends TokenRegexItListener {
 
   @override
   exitLiteral(context: TokenRegexItParser.LiteralContext): void {
-    console.log(context);
-    console.log(this.marker);
+    const { tokenIndex, text }: Symbol = context.STRING().symbol;
+    // pass position index up to parent, so TokenRegex node can get it as leftPosition or rightPosition
+    context.tokenIndex = tokenIndex;
+    this.marker.leaf(text, tokenIndex);
+  }
+
+  @override
+  exitTokenRegex(context: TokenRegexItParser.TokenRegexContext): void {
+    // For node that concat two tokens
+    if (context.getChildCount() === 2) {
+      const leftPosition = context.getChild(0).tokenIndex;
+      const rightPosition = context.getChild(1).tokenIndex;
+      context.tokenIndex = this.marker.concatPositionIndex(leftPosition, rightPosition);
+      this.marker.concat(leftPosition, rightPosition)
+    } else if (context.getChildCount() === 1 && context.getChild(0).symbol.type === -1) {
+      // EOF
+      const { tokenIndex } = context.getChild(0).symbol;
+      context.tokenIndex = tokenIndex;
+      this.marker.leaf(null, tokenIndex, true);
+    } else {
+      console.log('context.getChildCount()', context.getChildCount());
+    }
   }
 }
--- src/parse/PositionMarker.js
+++ src/parse/PositionMarker.js
@@ -1,8 +1,50 @@
+/* eslint-disable class-methods-use-this */
 // @flow
 import type { Marker } from './Listener';
 
 export default class PositionMarker implements Marker {
-  firstPosition(position: number) {
-    console.log(position);
+  positionSet: {
+    [position: string | number]: {
+      token?: string | null,
+      nullable: boolean,
+      first: number[],
+      last: number[],
+      follow: number[],
+    },
+  } = {};
+
+  /** evaluate firstPos, lastPos, nullable of a Leaf token */
+  leaf(token: string | null, position: number, nullable?: boolean = false): void {
+    const first = nullable ? [] : [position];
+    const last = nullable ? [] : [position];
+
+    this.positionSet[position] = {
+      token,
+      nullable,
+      first,
+      last,
+      follow: [],
+    };
+  }
+
+  concatPositionIndex(leftPosition: number, rightPosition: number): string {
+    return `${leftPosition}-${rightPosition}`;
+  }
+
+  /** evaluate firstPos, lastPos, nullable of a node concat several nodes */
+  concat(leftPosition: number, rightPosition: number): void {
+    const { nullable: leftNullable, first: leftFirst, last: leftLast } = this.positionSet[leftPosition];
+    const { nullable: rightNullable, first: rightFirst, last: rightLast } = this.positionSet[rightPosition];
+
+    const nullable = leftNullable || rightNullable;
+    const first = leftNullable ? [...leftFirst, ...rightFirst] : leftFirst;
+    const last = rightNullable ? [...leftLast, ...rightLast] : rightLast;
+
+    this.positionSet[this.concatPositionIndex(leftPosition, rightPosition)] = {
+      nullable,
+      first,
+      last,
+      follow: [],
+    };
   }
 }
